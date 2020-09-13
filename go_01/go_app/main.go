d8ea2e844a724859b4cfbb857e69c278c8d30a9f
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-13 22:09:43 +0900

    add models

--- go_01/go_app/main.go
+++ go_01/go_app/main.go
@@ -3,7 +3,38 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
+
+/* Models */
+
+// User ユーザ
+type User struct {
+	gorm.Model
+	Name string
+	Email string
+	Tasks []Task
+}
+// Project プロジェクト
+type Project struct {
+	gorm.Model
+	Name string
+	Description string
+	StartDate string
+	EndDate string
+	Tasks []Task
+}
+// Task タスク
+type Task struct {
+	gorm.Model
+	Title string
+	Description string
+	Status string
+	StartDate string
+	EndDate string
+	ProjectID uint
+	UserID uint
+}
 
 // HealthResponse Response - ヘルスチェック
 type HealthResponse struct {
